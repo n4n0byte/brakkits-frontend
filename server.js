7e203659f7c03d78e14c070f92a0666c4f47e5f7
Ali Cooper <ali.tarriq.cooper@gmail.com>
2020-11-01 19:04:59 -0700

    cors update with previous cors code removed

--- server.js
+++ server.js
@@ -5,17 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.use(cors());
-
-const { createProxyMiddleware } = require('http-proxy-middleware');
-app.use('/api', createProxyMiddleware({ 
-    target: 'http://localhost:8080/', //original url
-    changeOrigin: true, 
-    //secure: false,
-    onProxyRes: function (proxyRes, req, res) {
-       proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-    }
-}));
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
