972fcf312655515c89bdb561c34ad44e033dd80e
Ali Cooper <ali.tarriq.cooper@gmail.com>
2020-11-01 18:10:41 -0700

    cors update

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials());
+
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
